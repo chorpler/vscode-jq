416ccd78e09db5e3f9389e4ec4c50ca9a7a11c81
David Sargeant <admin@sesaonsite.com>
2022-10-19 17:34:03 -0500

    Only create `jq debug` panel if enabled in settings

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,14 @@
 import * as vscode from "vscode";
 
+const mockDebug = {
+  append: (val?:string) => {},
+  appendLine: (val?:string) => {},
+};
+
+const config = vscode.workspace.getConfiguration("jq");
+const { debugOutput } = config;
+
+export const Debug = debugOutput === true ? vscode.window.createOutputChannel("jq debug") : mockDebug;
 export const Logger = vscode.window.createOutputChannel("jq");
-export const Debug = vscode.window.createOutputChannel("jq debug");
 
 export default Logger;
